de4de0574e157b3a5db5b2b1c3e2ce6aad41c998
Taylor Hornby <havoc@defuse.ca>
2013-07-03 18:13:36 -0600

    Add captcha to register page.

--- www/register.php
+++ www/register.php
@@ -1,21 +1,34 @@
 <?php
 require_once('inc/account.php');
+require_once('inc/settings.php');
+require_once('inc/captcha.php');
 require_once('ui/layout.php');
 require_once('ui/register.php');
 
+$view = new RegisterView();
+$layout = new Layout($view);
+
 if (isset($_POST['submit'])) {
-    if ($_POST['password'] == $_POST['password_confirm']) {
-        try {
-            Account::CreateAccount($_POST['username'], $_POST['password']);
-        } catch (Exception $e) {
-            echo "<b>Something went wrong. $e</b>";
+    $captcha_good = true;
+    if (Settings::GetSetting('recaptcha.onregister') == "1") {
+        if (!Captcha::CheckCaptcha()) {
+            $layout->flash = "Incorrect captcha.";
+            $captcha_good = false;
         }
-    } else {
-        echo "<b>Passwords do not match.</b>";
+    }
+    if ($captcha_good) {
+        if ($_POST['password'] == $_POST['password_confirm']) {
+            try {
+                Account::CreateAccount($_POST['username'], $_POST['password']);
+                $layout->flash = "Account was successfully created. You may now log in.";
+            } catch (Exception $e) {
+                $layout->flash = "Something went wrong.";
+            }
+        } else {
+            $layout->flash = "The two passwords you provided do not match.";
+        }
     }
 }
 
-$view = new RegisterView();
-$layout = new Layout($view);
 $layout->show();
 ?>
--- www/ui/register.php
+++ www/ui/register.php
@@ -1,5 +1,7 @@
 <?php
 require_once('ui/view.php');
+require_once('inc/settings.php');
+require_once('inc/captcha.php');
 class RegisterView extends View
 {
     public function show()
@@ -9,6 +11,11 @@ class RegisterView extends View
     Username: <input type="text" name="username" value="" /> <br />
     Password: <input type="password" name="password" value="" /> <br />
     Confirm Password: <input type="password" name="password_confirm" value="" /> <br />
+    <?php
+        if (Settings::GetSetting('recaptcha.onregister') == "1") {
+            Captcha::ShowCaptcha();
+        }
+    ?>
     <input type="submit" name="submit" value="Create Account" />
 </form>
 <?
