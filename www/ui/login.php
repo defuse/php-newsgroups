abeeed0b66b21edf976a52f5a63789b00baa750f
Taylor Hornby <havoc@defuse.ca>
2013-07-06 15:47:36 -0600

    Align the login form with a table.

--- www/ui/login.php
+++ www/ui/login.php
@@ -7,11 +7,23 @@ class LoginView extends View
     public function show()
     {
 ?>
+    <div class="contentpadding">
     <form action="login.php" method="POST">
-    Username: <input type="text" name="username" value="<?php echo htmlentities($this->username, ENT_QUOTES); ?>" /><br />
-        Password: <input type="password" name="password" value="" /> <br />
-        <input type="submit" name="submit" value="Login" />
+        <table class="formalign">
+            <tr>
+                <td>Username:&nbsp;&nbsp;&nbsp;</td>
+                <td><input type="text" name="username" value="<?php echo htmlentities($this->username, ENT_QUOTES); ?>" /></td>
+            </tr>
+            <tr>
+                <td>Password:</td>
+                <td><input type="password" name="password" value="" /></td>
+            </tr>
+            <tr>
+                <td><input type="submit" name="submit" value="Login" /></td>
+            </tr>
+        </table>
     </form>
+    </div>
 <?
     }
 }
