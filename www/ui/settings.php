f7b4367746f0feb758c7db30b596ecd2c7d93733
Taylor Hornby <havoc@defuse.ca>
2013-07-06 15:30:02 -0600

    Align change password form with a table.

--- www/ui/settings.php
+++ www/ui/settings.php
@@ -5,19 +5,32 @@ class SettingsView extends View
     public function show()
     {
 ?>
-    <h1>Change Password</h1>
+    <div class="contentpadding">
+    <div id="othercontentsheader">
+        Change Password
+    </div>
     <form action="settings.php" method="POST">
-        Current Password:
-        <input type="password" name="old_password" />
-        <br />
-        New Password: 
-        <input type="password" name="new_password" />
-        <br />
-        Confirm New Password:
-        <input type="password" name="confirm_new_password" />
-        <br />
-        <input type="submit" name="change_password" value="Change Password" />
+        <table class="formalign">
+            <tr>
+                <td>Current Password:</td>
+                <td><input type="password" name="old_password" /></td>
+            </tr>
+            <tr>
+                <td>New Password:</td>
+                <td><input type="password" name="new_password" /></td>
+            </tr>
+            <tr>
+                <td>Confirm New Password:&nbsp;&nbsp;&nbsp;</td>
+                <td><input type="password" name="confirm_new_password" /></td>
+            </tr>
+            <tr>
+                <td>
+                <input type="submit" name="change_password" value="Change Password" />
+                </td>
+            </tr>
+        </table>
     </form>
+    </div>
 <?
     }
 }
