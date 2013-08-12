2ebbf4851252042ea919d363ecdf4a8bb84d0c9e
redragonx <stephen@dicesoft.net>
2013-08-12 08:57:02 -0600

    Update mysql.php
    disable emulating prepared statements.

--- www/inc/mysql.php
+++ www/inc/mysql.php
@@ -3,6 +3,7 @@ require_once('/etc/creds.php');
 
 $DB = get_db();
 $DB->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_WARNING);
+$DB->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
     
 function get_db()
 {
