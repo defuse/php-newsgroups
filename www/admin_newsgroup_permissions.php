73c553df0cb4a888da6b7fdeef73ce04ca1fe85a
Taylor Hornby <havoc@defuse.ca>
2013-09-22 13:25:39 -0600

    Implement newsgroup/usergroup admin page. #2

--- www/admin_newsgroup_permissions.php
+++ www/admin_newsgroup_permissions.php
@@ -3,6 +3,7 @@ require_once('ui/admin/newsgroup_permissions.php');
 require_once('ui/layout.php');
 require_once('inc/permissions.php');
 require_once('inc/Newsgroup.php');
+Login::RequireAdmin('index.php');
 
 $view = new AdminNewsgroupPermissionsView();
 $layout = new Layout($view);
@@ -22,6 +23,37 @@ if ($view->newsgroup && isset($_POST['set_anonymous_access'])) {
         $layout->flash = "Anonymous access saved.";
     } catch (InvalidAccessLevelException $e) {
         $layout->flash = "Invalid access level selection.";
+    }
+}
+
+if ($view->newsgroup && isset($_POST['add_user_group'])) {
+    try {
+        $name = $_POST['user_group_name'];
+        $user_group = UserGroup::GetUserGroupByName($name);
+        $user_group->setAccessToNewsgroup($view->newsgroup, 'NOACCESS');
+        $layout->flash = "Added.";
+    } catch (UserGroupDoesNotExistException $e) {
+        $layout->flash = "User group does not exist.";
+    }
+}
+
+if ($view->newsgroup && isset($_POST['remove_user_group'])) {
+    try {
+        $user_group = new UserGroup($_POST['user_group_id']);
+        $user_group->removeExplicitAccessToNewsgroup($view->newsgroup);
+    } catch (UserGroupDoesNotExistException $e) {
+        $layout->flash = "User group does not exist.";
+    }
+}
+
+if ($view->newsgroup && isset($_POST['set_access_level'])) {
+    try {
+        $user_group = new UserGroup($_POST['user_group_id']);
+        $user_group->setAccessToNewsgroup($view->newsgroup, $_POST['access_level']);
+    } catch (UserGroupDoesNotExistException $e) {
+        $layout->flash = "User group does not exist.";
+    } catch (InvalidAccessLevelException $e) {
+        $layout->flash = "Invalid access level.";
     }
 }
 
--- www/ui/admin/newsgroup_permissions.php
+++ www/ui/admin/newsgroup_permissions.php
@@ -43,6 +43,63 @@ class AdminNewsgroupPermissionsView extends View
 
         <h2>User Groups</h2>
 
+        <table>
+            <tr>
+                <th>User Group</th>
+                <th>Access</th>
+                <th>Remove</th>
+            </tr>
+            <?php
+                $user_groups = UserGroup::GetAllUserGroupsForNewsgroup($this->newsgroup);
+                foreach ($user_groups as $usergroup) {
+
+                    $safe_name = htmlentities($usergroup->getName(), ENT_QUOTES);
+                    $safe_id = htmlentities($usergroup->getID(), ENT_QUOTES);
+
+                    $access = $usergroup->getAccessToNewsgroup($this->newsgroup);
+                    $selected = 'selected="selected"';
+                    $sel_NOACCESS = $access == 'NOACCESS' ? $selected : "";
+                    $sel_READONLY = $access == 'READONLY' ? $selected : "";
+                    $sel_READWRITECAPTCHA = $access == 'READWRITECAPTCHA' ? $selected : "";
+                    $sel_READWRITE = $access == 'READWRITE' ? $selected : "";
+                ?>
+                    <tr>
+                        <td><?php echo $safe_name; ?></td>
+                        <td>
+                            <form action="admin_newsgroup_permissions.php?newsgroup=<?php echo htmlentities($this->newsgroup->getName(), ENT_QUOTES);?>" method="POST">
+                            <select name="access_level">
+                                <option value="NOACCESS" <?php echo $sel_NOACCESS; ?>>No access</option>
+                                <option value="READONLY" <?php echo $sel_READONLY; ?>>Read only</option>
+                                <option value="READWRITECAPTCHA" <?php echo $sel_READWRITECAPTCHA; ?>>Read/Write with CAPTCHA</option>
+                                <option value="READWRITE" <?php echo $sel_READWRITE; ?>>Read/Write</option>
+                            </select>
+                            <input type="hidden" name="user_group_id" value="<?php echo $safe_id; ?>" />
+                            <input type="submit" name="set_access_level" value="Save" />
+                            </form>
+                        </td>
+                        <td>
+                            <form action="admin_newsgroup_permissions.php?newsgroup=<?php echo htmlentities($this->newsgroup->getName(), ENT_QUOTES);?>" method="POST">
+                                <input type="submit" name="remove_user_group" value="Remove" />
+                                <input type="hidden" name="user_group_id" value="<?php echo $safe_id; ?>" />
+                            </form>
+                        </td>
+                    </tr>
+                <?
+                }
+            ?>
+        </table>
+
+        <br /><br />
+
+        <form action="admin_newsgroup_permissions.php?newsgroup=<?php echo htmlentities($this->newsgroup->getName(), ENT_QUOTES);?>" method="POST">
+            <table class="formalign">
+                <tr>
+                    <td>Add:</td>
+                    <td><input type="text" name="user_group_name" value="" /></td>
+                    <td><input type="submit" name="add_user_group" value="Add" /></td>
+                </tr>
+            </table>
+        </form>
 
     <? } ?>
     </div>
